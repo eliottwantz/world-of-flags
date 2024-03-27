7549bda0f60091cef76e8fd2228de62c0435a20b
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2024-03-26 22:24:50 -0400

    nice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,16 @@
 	<title>World of Flags</title>
 </svelte:head>
 
-<main>
-	<pre>
-		{JSON.stringify(data.flags, null, 2)}
-	</pre>
+<main class="mx-auto flex min-h-svh max-w-screen-lg flex-col py-4">
+	<header>
+		<h1 class="text-center text-5xl font-semibold leading-10">World of Flags</h1>
+	</header>
+
+	<div class="flex h-full flex-1 items-center justify-center">
+		<div>
+			<p class="text-xl">
+				Select a country to see the flag of the country and the language spoken in that country
+			</p>
+		</div>
+	</div>
 </main>
