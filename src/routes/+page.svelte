76cb3907ced88e8c1a5d546be8cae6a5a24ccd1b
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2024-03-29 19:16:01 -0400

    ok should be good for v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,8 +13,6 @@
 		<h1 class="text-5xl font-semibold leading-10">World of Flags</h1>
 	</header>
 
-	<a href={base}>{base}</a>
-
 	<div class="flex flex-col items-center gap-4">
 		<img class="max-w-lg" src="{base}/landing-page.jpg" alt="a lot of flags" />
 		<div class="flex flex-col gap-2">
@@ -26,10 +24,12 @@
 				class="scale-100 transform rounded-lg border-2 border-yellow-400 bg-yellow-400/10 p-2 shadow-md transition duration-100 ease-in-out hover:bg-yellow-400/20 active:scale-90"
 				>New Game</button
 			>
-			<a
-				href="{base}/game"
+			<button
+				on:click={() => {
+					goto(`${base}/game`);
+				}}
 				class="scale-100 transform rounded-lg border-2 border-yellow-400/60 bg-yellow-400/10 p-2 shadow-md transition duration-100 ease-in-out hover:bg-yellow-400/20 active:scale-90"
-				>Resume</a
+				>Resume</button
 			>
 		</div>
 	</div>
