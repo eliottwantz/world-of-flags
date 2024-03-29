fc66552f2b4d5a6966b3f7243c7f936c66176211
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2024-03-29 19:12:43 -0400

    Updates

---
+++ _app/immutable/chunks/paths.dfzbnG5e.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1x3e0ba)==null?void 0:s.base)??"/world-of-flags";var a;const t=((a=globalThis.__sveltekit_1x3e0ba)==null?void 0:a.assets)??e;export{t as a,e as b};
