8faf342b75e6a3c0df71ed8c94cc2198ca47a214
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2024-04-20 11:19:06 -0400

    working map

--- src/lib/components/Map/Map.svelte
+++ src/lib/components/Map/Map.svelte
@@ -1,35 +1,46 @@
 <script lang="ts">
+	import geojsonData from '$lib/geojson/countries.json';
 	import type { GeoJsonObject } from 'geojson';
-	import L from 'leaflet';
+	import L, { type LeafletMouseEventHandlerFn } from 'leaflet';
 	import 'leaflet/dist/leaflet.css';
-	import type { MouseEventHandler } from 'svelte/elements';
 
-	let map = $state<L.Map | undefined>(undefined);
+	let mapL = $state<L.Map | undefined>(undefined);
 
 	$effect(() => {
-		const mapL = L.map('map');
-		mapL.setView([15, 0], 2);
+		console.log(geojsonData);
+		const map = L.map('map');
+		map.setView([15, 0], 2);
 
 		L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
 			attribution:
 				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 			subdomains: 'abcd',
 			maxZoom: 20
-		}).addTo(mapL);
+		}).addTo(map);
 
-		map = mapL;
+		//@ts-ignore
+		L.geoJSON(geojsonData.features, {
+			onEachFeature: (feature, layer) => {
+				layer.on({
+					click: handleClick
+				});
+			}
+		}).addTo(map);
+
+		mapL = map;
 	});
 
-	const handleClick: MouseEventHandler<HTMLDivElement> = (event) => {
-		if (!map) return;
-		const latlng = map.mouseEventToLatLng(event);
-		// Here you would compare latlng with the coordinates of each country
-		// to determine which country was clicked and provide appropriate feedback.
-		console.log('Clicked at:', latlng);
+	const handleClick: LeafletMouseEventHandlerFn = (event) => {
+		const properties = event.target.feature.properties as {
+			ADMIN: string;
+			ISO_A3: string;
+			ISO_A2: string;
+		};
+		console.log('Clicked country:', properties.ADMIN);
 	};
 </script>
 
-<div id="map" onclick={handleClick}></div>
+<div id="map"></div>
 
 <style>
 	#map {
