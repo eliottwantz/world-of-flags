300e1959906fe8ed05235a73b3ee5530cff16252
Eliott Wantz <eliott.wantz@develiott.com>
2025-06-20 09:24:29 -0400

    adapter cloudflare for workers with static binding

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,17 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-static for SSG
-		adapter: adapter({
-			pages: "build",
-			assets: "build",
-			fallback: undefined,
-			precompress: false,
-			strict: true,
-		}),
-		prerender: {
-			handleHttpError: "warn",
-		},
+		adapter: adapter(),
 	},
 };
 
